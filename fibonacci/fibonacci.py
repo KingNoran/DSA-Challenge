f05b118f9ebe40a8085f3bf755e7d6f8aedea1fa
KingNoran <kingnoran8@gmail.com>
2024-05-31 14:08:43 +0800

    polished fibonacci!

--- fibonacci/fibonacci.py
+++ fibonacci/fibonacci.py
@@ -3,7 +3,7 @@ n = 10
 
 for i in range(n):
     if(i > 1):
-        fibonacci.append(fibonacci[len(fibonacci) - 1] + fibonacci[len(fibonacci) - 2])
+        fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
     else:
         fibonacci.append(i)
 
